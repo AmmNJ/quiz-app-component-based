f20cbbf92324effe18aea607bfb1560f28ee88d5
Ammersken Nils <nilsammersken@gmail.com>
2021-02-04 12:51:51 +0100

    Add button to storybook

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import Button from './components/Button'
 
-const button = Button('get in Touch')
+const button = Button('!Not Agree')
+const button2 = Button('Agree')
+
 document.body.append(button)
+document.body.append(button2)
