effea90a5d70cc0a0eed704ba59237945a9ae6ab
Ammersken Nils <nilsammersken@gmail.com>
2021-02-04 14:15:17 +0100

    Add Header (git case-sensitive commit)

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,12 @@
+import './Header.css'
+
+export default function Header() {
+  const el = document.createElement('Header')
+  el.className = Header
+  el.innerHTML = `
+    <h1 class="Header__title">
+    Quiz App
+    <span class="Header__subtitle">May the best win!</span>
+    </h1>`
+  return el
+}
