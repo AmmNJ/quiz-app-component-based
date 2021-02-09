f4ad495961f3a17310bf98b71f145cf09ce0e448
Ammersken Nils <nilsammersken@gmail.com>
2021-02-09 14:57:06 +0100

    Add pages navigation & onSubmit event

--- src/index.js
+++ src/index.js
@@ -1,27 +1,51 @@
 import Card from './components/Card'
+import CreateForm from './components/CreateForm'
 import Header from './components/Header'
 import Navigation from './components/Navigation'
 import createElement from './lib/createElement'
 
-const { el: headerEl, setText: setHeaderText } = Header(
-  'Quiz App',
-  'May the best win!'
+const navigation = Navigation(onNavigate)
+const form = CreateForm(onSubmit)
+
+const { el: headerEl } = Header('Quiz App', 'May the best win!')
+
+const homePage = createElement(
+  'main',
+  { className: 'HomePage', hidden: false },
+  Card('Foo bar', 'Baz bar!'),
+  Card('Foo bar', 'Baz bar!'),
+  Card('Foo bar', 'Baz bar!')
 )
 
-const navigation = Navigation(onNavigate)
-
-function onNavigate(text) {
-  console.log(text)
-}
+const createPage = createElement(
+  'main',
+  { className: 'CreatePage', hidden: true },
+  form
+)
 
 const grid = createElement(
   'div',
   { className: 'appGrid' },
   headerEl,
-  Card('Foo bar?', 'Bar baz!'),
+  homePage,
+  createPage,
   navigation
 )
 
-setHeaderText('Create', 'a new card')
-
 document.body.append(grid)
+
+function onSubmit(question, answer) {
+  homePage.append(Card(question, answer))
+}
+
+function onNavigate(text) {
+  if (text === 'Home') {
+    homePage.hidden = false
+    createPage.hidden = true
+  }
+
+  if (text === 'Create') {
+    homePage.hidden = true
+    createPage.hidden = false
+  }
+}
